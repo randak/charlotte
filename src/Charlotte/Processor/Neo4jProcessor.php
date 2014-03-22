afc3875e4c573405a52405163912c05b54dfc3b5
Kristian Randall <kristian.l.randall@gmail.com>
2014-03-22 00:07:06 -0700

    Fixes #3. Added support for a YAML configuration file, allowing for definition of database connections and URL exclusions. The starting URL definition has been moved to here, though it may still be set manually.

--- src/Charlotte/Processor/Neo4jProcessor.php
+++ src/Charlotte/Processor/Neo4jProcessor.php
@@ -6,139 +6,139 @@ use Everyman\Neo4j\Cypher\Query;
 
 class Neo4jProcessor extends Processor
 {
-	protected function query($str, $vars = array())
-	{
-		if(!isset($this->connector)) {
-			die("No database is connected. Aborting query.");
-		}
+    protected function query($str, $vars = array())
+    {
+        if(!isset($this->connector)) {
+            die("No database is connected. Aborting query.");
+        }
 
-		$query = new Query($this->connector, $str, $vars);
-		$query->getResultSet();
-	}
+        $query = new Query($this->connector, $str, $vars);
+        $query->getResultSet();
+    }
 
-	protected function urlQuery($url, $title)
-	{
-		$str = "MERGE (n:Page {url: {url} }) 
-				SET n.title = {title}";
-				// ON CREATE SET n.updateTime=timestamp(), n.createTime=timestamp(), n.title={title}
-				// ON MATCH SET n.title={title}';
+    protected function urlQuery($url, $title)
+    {
+        $str = "MERGE (n:Page {url: {url} })
+                SET n.title = {title}";
+                // ON CREATE SET n.updateTime=timestamp(), n.createTime=timestamp(), n.title={title}
+                // ON MATCH SET n.title={title}';
 
-		$vars = array(
-					'url'  => $url,
-					'title' => $title
-				);
+        $vars = array(
+                    'url'  => $url,
+                    'title' => $title
+                );
 
-		$this->query($str, $vars);
-	}
+        $this->query($str, $vars);
+    }
 
-	protected function metaQuery($url, $metas)
-	{
-		$description = isset($metas["description"]) ? $metas["description"] : "";
-		$keywords = isset($metas["keywords"]) ? $metas["keywords"] : "";
+    protected function metaQuery($url, $metas)
+    {
+        $description = isset($metas["description"]) ? $metas["description"] : "";
+        $keywords = isset($metas["keywords"]) ? $metas["keywords"] : "";
 
-		$str = "MATCH (n:Page {url: {url} })
-				SET n.description={description}, n.keywords={keywords}";
+        $str = "MATCH (n:Page {url: {url} })
+                SET n.description={description}, n.keywords={keywords}";
 
-		$vars = array(
-					"url" => $url,
-					"description" => $description,
-					"keywords" => $keywords
-				);
+        $vars = array(
+                    "url" => $url,
+                    "description" => $description,
+                    "keywords" => $keywords
+                );
 
-		$this->query($str, $vars);
-	}
+        $this->query($str, $vars);
+    }
 
-	protected function keywordQuery($url, $keywords)
-	{
-		foreach($keywords as $key => $value) {
-			$str = "MERGE (n:Page {url: {url} })
-					MERGE (m:Keyword {word: {word} })
-					MERGE (n)-[:CONTAINS_KEYWORD {frequency: {freq}}]->(m)";
+    protected function keywordQuery($url, $keywords)
+    {
+        foreach($keywords as $key => $value) {
+            $str = "MERGE (n:Page {url: {url} })
+                    MERGE (m:Keyword {word: {word} })
+                    MERGE (n)-[:CONTAINS_KEYWORD {frequency: {freq}}]->(m)";
 
-			$vars = array(
-						"url" => $url,
-						"word" => $key,
-						"freq" => $value
-					);
+            $vars = array(
+                        "url" => $url,
+                        "word" => $key,
+                        "freq" => $value
+                    );
 
-			$this->query($str, $vars);
-		}
-	}
+            $this->query($str, $vars);
+        }
+    }
 
-	protected function h1Query($url, $h1s)
-	{
-		$h1s = "[" . implode("\", \"", $h1s) . "]";
-		$h1s = str_replace(array("\r\n", "\n", "\r"), ' ', $h1s);
+    protected function h1Query($url, $h1s)
+    {
+        $h1s = "\"[" . implode("\", \"", $h1s) . "]\"";
+        $h1s = str_replace(array("\r\n", "\n", "\r"), ' ', $h1s);
 
-		$str = "MATCH (n:Page {url: {url} })
-				SET n.h1={h1s}";
-				//SET n.updateTime=timestamp(), n.h1s={h1s}";
+        $str = "MERGE (n:Page {url: {url} })
+                SET n.h1={headers}";
+                //SET n.updateTime=timestamp(), n.h1s={h1s}";
 
-		$vars = array(
-					'url' => $url,
-					'h1s'  => $h1s
-				);
+        $vars = array(
+                    'url' => $url,
+                    'headers'  => $h1s
+                );
 
-		$this->query($str, $vars);
-	}
+        $this->query($str, $vars);
+    }
 
-	protected function scriptQuery($url, $scripts)
-	{
-		foreach($scripts as $script) {
-			$str = "MERGE (n:Page {url:{url}}) 
-					MERGE (s:Script {src:{src}, content:{content}}) 
-					MERGE (n)-[:CONTAINS_SCRIPT {location: {location}}]->(s)";
-					// ON CREATE SET s.updateTime=timestamp(), s.createTime=timestamp()
-					// ON MATCH SET s.updateTime=timestamp(), n.updateTime=timestamp()";
+    protected function scriptQuery($url, $scripts)
+    {
+        foreach($scripts as $script) {
+            $str = "MERGE (n:Page {url:{url}})
+                    MERGE (s:Script {src:{src}, content:{content}})
+                    MERGE (n)-[:CONTAINS_SCRIPT {location: {location}}]->(s)";
+                    // ON CREATE SET s.updateTime=timestamp(), s.createTime=timestamp()
+                    // ON MATCH SET s.updateTime=timestamp(), n.updateTime=timestamp()";
 
-			$location = empty($script["src"]) ? "inline" : "external";
-			$vars = array(
-						'url'   	=> $url,
-						'src' 		=> $script["src"],
-						'content' 	=> $script["content"],
-						'location'	=> $location
-					);
+            $location = empty($script["src"]) ? "inline" : "external";
+            $vars = array(
+                        'url'   	=> $url,
+                        'src' 		=> $script["src"],
+                        'content' 	=> $script["content"],
+                        'location'	=> $location
+                    );
 
-			$this->query($str, $vars);
-		}
-	}
+            $this->query($str, $vars);
+        }
+    }
 
-	protected function stylesheetQuery($url, $stylesheets)
-	{
-		foreach($stylesheets as $stylesheet) {
-			$str = "MERGE (n:Page {url: {url} }) 
-					MERGE (s:StyleSheet {href: {href}, media: {media}, type: {type} }) 
-					MERGE (n)-[:CONTAINS_STYLESHEET]->(s)";
-					// ON CREATE SET s.updateTime=timestamp(), s.createTime=timestamp()
-					// ON MATCH SET s.updateTime=timestamp(), n.updateTime=timestamp()';
+    protected function stylesheetQuery($url, $stylesheets)
+    {
+        foreach($stylesheets as $stylesheet) {
+            $str = "MERGE (n:Page {url: {url} })
+                    MERGE (s:StyleSheet {href: {href}, media: {media}, type: {type} })
+                    MERGE (n)-[:CONTAINS_STYLESHEET]->(s)";
+                    // ON CREATE SET s.updateTime=timestamp(), s.createTime=timestamp()
+                    // ON MATCH SET s.updateTime=timestamp(), n.updateTime=timestamp()';
 
-			$vars = array(
-						"url"	=> $url,
-						"href" 	=> $stylesheet["href"],
-						"media" => $stylesheet["media"],
-						"type" 	=> $stylesheet["type"]
-					);
+            $vars = array(
+                        "url"	=> $url,
+                        "href" 	=> $stylesheet["href"],
+                        "media" => $stylesheet["media"],
+                        "type" 	=> $stylesheet["type"]
+                    );
 
-			$this->query($str, $vars);
-		}
-	}
+            $this->query($str, $vars);
+        }
+    }
 
-	protected function linksQuery($url, $links)
-	{
-		foreach($links as $link) {
-			$str = 'MERGE (n:Page {url: {url} }) 
-					MERGE (m:Page {url: {link} }) 
-					MERGE (n)-[:LINKS_TO {text: {text} }]->(m)
-					ON CREATE SET m.updateTime=timestamp(), m.createTime=timestamp()
-					ON MATCH SET n.updateTime=timestamp(), m.updateTime=timestamp()';
+    protected function linksQuery($url, $links)
+    {
+        foreach($links as $link) {
+            $str = 'MERGE (n:Page {url: {url} })
+                    MERGE (m:Page {url: {link} })
+                    MERGE (n)-[:LINKS_TO {text: {text} }]->(m)
+                    ON CREATE SET m.updateTime=timestamp(), m.createTime=timestamp()
+                    ON MATCH SET n.updateTime=timestamp(), m.updateTime=timestamp()';
 
-			$vars = array(
-						'url' 	=> $url,
-						'link' 	=> $link["href"],
-						'text'	=> $link["text"]
-					);
+            $vars = array(
+                        'url' 	=> $url,
+                        'link' 	=> $link["href"],
+                        'text'	=> $link["text"]
+                    );
 
-			$this->query($str, $vars);
-		}
-	}
+            $this->query($str, $vars);
+        }
+    }
 }
--- src/Charlotte/Processor/Processor.php
+++ src/Charlotte/Processor/Processor.php
@@ -3,40 +3,40 @@
 namespace Charlotte\Processor;
 
 abstract class Processor
-{	
-	protected $connector;
+{
+    protected $connector;
 
-	public function __construct($connector = '')
-	{
-		if(!empty($connector)) {
-			$this->setConnector($connector);
-		}
-	}
+    public function __construct($connector = '')
+    {
+        if(!empty($connector)) {
+            $this->setConnector($connector);
+        }
+    }
 
-	public function setConnector($connector)
-	{
-		$this->connector = $connector;
-	}
+    public function setConnector($connector)
+    {
+        $this->connector = $connector;
+    }
 
-	public function process($page)
-	{
-		if(!empty($page["url"])) {
-			$this->urlQuery($page["url"], $page["title"]);
-			if(!empty($page["metas"])) $this->metaQuery($page["url"], $page["metas"]);
-			if(!empty($page["h1s"])) $this->h1Query($page["url"], $page["h1s"]);
-			if(!empty($page["scripts"])) $this->scriptQuery($page["url"], $page["scripts"]);
-			if(!empty($page["stylesheets"])) $this->stylesheetQuery($page["url"], $page["stylesheets"]);
-			if(!empty($page["keywords"])) $this->keywordQuery($page["url"], $page["keywords"]);
-			if(!empty($page["links"])) $this->linksQuery($page["url"], $page["links"]);
-		}
-	}
+    public function process($page)
+    {
+        if(!empty($page["url"])) {
+            $this->urlQuery($page["url"], $page["title"]);
+            if(!empty($page["metas"])) $this->metaQuery($page["url"], $page["metas"]);
+            if(!empty($page["h1s"])) $this->h1Query($page["url"], $page["h1s"]);
+            if(!empty($page["scripts"])) $this->scriptQuery($page["url"], $page["scripts"]);
+            if(!empty($page["stylesheets"])) $this->stylesheetQuery($page["url"], $page["stylesheets"]);
+            if(!empty($page["keywords"])) $this->keywordQuery($page["url"], $page["keywords"]);
+            if(!empty($page["links"])) $this->linksQuery($page["url"], $page["links"]);
+        }
+    }
 
-	abstract protected function query($str, $vars = array());
-	abstract protected function urlQuery($url, $title);
-	abstract protected function metaQuery($url, $metas);
-	abstract protected function keywordQuery($url, $keywords);
-	abstract protected function h1Query($url, $headers);
-	abstract protected function scriptQuery($url, $scripts);
-	abstract protected function stylesheetQuery($url, $stylesheets);
-	abstract protected function linksQuery($url, $links);
+    abstract protected function query($str, $vars = array());
+    abstract protected function urlQuery($url, $title);
+    abstract protected function metaQuery($url, $metas);
+    abstract protected function keywordQuery($url, $keywords);
+    abstract protected function h1Query($url, $headers);
+    abstract protected function scriptQuery($url, $scripts);
+    abstract protected function stylesheetQuery($url, $stylesheets);
+    abstract protected function linksQuery($url, $links);
 }
